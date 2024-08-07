70b9cd7b387d37a092ebc8ee92bc157dc6d7558f
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-08-07 13:00:39 +0530

    create navbar and fix css

--- webfull-vision/src/App.jsx
+++ webfull-vision/src/App.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import "./style.css"
 import HomePage from "./pages/home-page";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 const App = () =>{
     return(
         <BrowserRouter>
