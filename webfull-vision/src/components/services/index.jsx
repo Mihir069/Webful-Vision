8e245d84a0ff33720e709dc7c2500cf5019a0120
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-08-10 15:51:49 +0530

    create service banner

--- webfull-vision/src/components/services/index.jsx
+++ webfull-vision/src/components/services/index.jsx
@@ -1,7 +1,10 @@
-const Services = () =>{
-    return(
-        <div>
-            kjrfberfbiknbjh 
+const Services = () => {
+    return (
+        <div className="relative">
+            <div className="relative h-40"><img src="/img/footer-bg.jpg" alt="footer-bg" className="w-full h-full object-cover brightness-75" /> </div>
+            <div className="absolute inset-0 flex items-center justify-center px-48">
+                <p className="text-white text-3xl font-bold">Text</p>
+            </div>
         </div>
     )
 }
