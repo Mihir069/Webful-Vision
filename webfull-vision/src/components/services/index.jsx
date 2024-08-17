f3b9b97007afc32c3dbc1b2ded1955eb19e1185d
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-08-17 19:19:06 +0530

    fix responsivness & shorten the service component

--- webfull-vision/src/components/services/index.jsx
+++ webfull-vision/src/components/services/index.jsx
@@ -1,13 +1,13 @@
 import ServicesCard from "../service-card";
 
 const Services = () => (
-  <div className="relative bg-[url('/img/footer-bg.jpg')] bg-cover bg-center h-auto text-center px-48">
+  <div className="relative bg-[url('/img/footer-bg.jpg')] bg-cover bg-center h-auto text-center px-4 sm:px-8 lg:px-48">
     <div className="absolute inset-0 bg-black opacity-70 z-0"></div>
-    <div className="relative z-10 px-20 py-14">
-      <h1 className="text-slate-100 text-4xl uppercase font-bold">
+    <div className="relative z-10 px-4 sm:px-8 py-10 sm:py-14">
+      <h1 className="text-slate-100 text-2xl sm:text-4xl uppercase font-bold">
         Our Eye Services
       </h1>
-      <p className="text-md text-slate-100">
+      <p className="text-sm sm:text-md text-slate-100">
         Our Services To Give Best Care For Your Eyes
       </p>
     </div>
