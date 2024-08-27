6398ece098a87cda74bc19cbb76aa68a492120cd
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-08-27 19:02:47 +0530

    fix responsiveness

--- webfull-vision/src/components/main-navbar/index.jsx
+++ webfull-vision/src/components/main-navbar/index.jsx
@@ -4,26 +4,51 @@ import navBarList from "../../../data/mainNavBar.json";
 
 const MainNavBar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsMenuOpen((prevState) => !prevState);
-  };
+  const toggleMenu = () => setIsMenuOpen((prevState) => !prevState);
+
+  const renderNestedMenu = (nestedItems) => (
+    <ul className="absolute  lg:left-0 md:right-0 top-full mt-1 z-[100] bg-[#CE6021] shadow-lg w-48 hidden group-hover:block">
+      {nestedItems.map((item) => (
+        <li key={item.id} className="relative px-5 py-2 hover:bg-[#15568E]">
+          <Link
+            to={item.path}
+            className="text-slate-100 font-customFont text-sm"
+          >
+            <span className="text-xs pr-2">{">>"}</span> {item.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+
   const navBarMenu = navBarList.map((item) => (
-    <Link
-      to={item.path}
+    <li
       key={item.id}
-      className="relative py-3 px-3 w-full hover:bg-[#CE6021] cursor-pointer"
+      className={`relative py-3 px-3 w-full ${
+        item.more ? "group" : ""
+      } hover:bg-[#CE6021] cursor-pointer`}
     >
-      <li className="text-slate-100 text-lg uppercase w-full text-left">
+      <Link
+        to={item.path}
+        className="text-slate-100 text-lg uppercase w-full text-left"
+      >
         {item.name}
-      </li>
-    </Link>
+      </Link>
+      {item.more && renderNestedMenu(item.more)}
+    </li>
   ));
 
   const navBarBmenu = navBarList.map((item) => (
-    <li key={item.id} className="relative px-5 py-3 hover:bg-[#CE6021]">
+    <li
+      key={item.id}
+      className={`relative px-5 py-3 cursor-pointer ${
+        item.more ? "group" : ""
+      } hover:bg-[#CE6021]`}
+    >
       <Link to={item.path} className="text-slate-100 uppercase">
         {item.name}
       </Link>
+      {item.more && renderNestedMenu(item.more)}
     </li>
   ));
 
