f5a70afb5e6e8cdf479ff036d910f99c1aa2c65f
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-09-02 21:38:30 +0530

    create FAQ component

--- webfull-vision/src/App.jsx
+++ webfull-vision/src/App.jsx
@@ -6,12 +6,14 @@ const HomePage = lazy(() => import("./pages/home-page/home-page-version-1"));
 const HomePageVersion2 = lazy(() =>
   import("./pages/home-page/home-page-version-2")
 );
+const FaqPage = lazy(() => import("./pages/faq-page"));
 const App = () => (
   <BrowserRouter>
     <Suspense fallback={<div>loading....</div>}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/home-version-2" element={<HomePageVersion2 />} />
+        <Route path="/faq" element={<FaqPage />} />
       </Routes>
     </Suspense>
   </BrowserRouter>
