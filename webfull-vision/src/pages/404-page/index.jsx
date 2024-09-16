dc8ab4da743ee81bf101f0f4344cc4f7ee74e08b
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-09-16 18:45:55 +0530

    shorten the 404 error component page

--- webfull-vision/src/pages/404-page/index.jsx
+++ webfull-vision/src/pages/404-page/index.jsx
@@ -1,13 +1,20 @@
+import PageBanner from "../../components/common/page-banner";
 import Connect from "../../components/connect";
 import Error from "../../components/error";
-import ErrorBanner from "../../components/error/error-banner";
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 
 const ErrorPage = () => (
   <>
     <Header />
-    <ErrorBanner />
+    <PageBanner
+      title="404 Error!"
+      breadcrumbs={[
+        { label: "Home", path: "/" },
+        { label: "Gene Splicing", path: "/" },
+        { label: "Cloning", path: "/" },
+      ]}
+    />
     <Error />
     <Connect />
     <Footer />
